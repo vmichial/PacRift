47f74ee575aa54b8c8c66d905633761e6cbb662f
BananasGoMoo <snowballs3776573@aol.com>
2014-02-23 19:03:29 -0800

    Updated map generator code
    now it should be able to find the "Begin Map File" character. I also
    made the PlaceBlocks function actually place the block it receives, even
    though I actually don't call that function yet.

--- Assets/Maps/MapGen.cs
+++ Assets/Maps/MapGen.cs
@@ -5,7 +5,6 @@ using System.IO; //for files and stuff
 public class MapGen : MonoBehaviour { //pre-generated class
 
 	/* Globals: 
-	 * should just be the file stream and file path(s). 
 	 * Only has one file path for now, 
 	 * but I was thinking of having all the 
 	 * maps we make included in the exe, 
@@ -15,48 +14,89 @@ public class MapGen : MonoBehaviour { //pre-generated class
 	
 	string fname;
 	System.IO.StreamReader file;
-	
-	string FindToken(string target, string input) {
-
-
-	}
+	string tokens = "@#/\\{};|&:";
+	int checkToken = -1;
 
 	//this will place the blocks/prefabs for the generator.
-	bool PlaceBlocks (GameObject prefab) {
+	bool PlaceBlocks (GameObject prefab, double x, double y, double z) {
+		GameObject block = (GameObject)Instantiate(Resources.Load(prefab)); //Apparently returns 'null' if no prefab of that name/path exists.
+		if (block == null) { 
+			Debug.LogError("Error: Prefab name is invalid. Prefab not found.");
+			return false;
+		} else {
+			//PLACE BLOCKS!!
+			block.transform.position.Set(x, y, z);
+		}
 		//if placed correctly (no issue finding the prefab, numbers are valid, etc.) this will return true.
 		return true;
 	}
 
-	public void CreateMap (StreamReader mapFile) { //this will generate the map. will call PlaceBlocks every time it needs to. 
+	public void CreateMap (StreamReader mapFile) { //this will generate the map. will call PlaceBlocks every time it needs to.
+
+		//!!! See PRM_Custom1.txt (in the same dir as this file) for the map file syntax...
 		
 		string input;
-		System.IO.StreamReader ss = mapFile as StreamReader;
-		while((input = ss.ReadLine()) != null) {
-			Debug.Log(input);
+		string currentText;
+		System.IO.StreamReader ss = mapFile;
+		bool fileStart = false;
+		while((input = ss.ReadLine()) != null) { //While not EOF
+			checkToken = input.IndexOfAny(tokens.ToCharArray()); //check for any key character
+			if (checkToken == -1) { //No keywords found
+				Debug.Log("No matching tokens left!");
+				continue;
+			}
+			currentText = input.Substring(checkToken); //substring starting with character found
+
+			if (fileStart == false) { //have I found the '{' character yet?
+				while (!currentText[0].Equals('{')){ //while its not found
+					checkToken = input.IndexOfAny(tokens.ToCharArray()); //get first char
+					if (checkToken == -1) { //No more keywords found
+						Debug.Log("No \'(\' token found yet! Reading next line...");
+						break;
+					} else { //now, look for a '{'
+						currentText = input.Substring(checkToken);
+						if (currentText[0].Equals('{')) { //Found one!
+							fileStart = true;
+						}
+
+					}
+
+				} // end of "check for first '{' character" section
+
+
+			} else if (fileStart == true){ //now look for anything else
+				checkToken = input.IndexOfAny(tokens.ToCharArray());
+
+			}
+
 			
 		}
-		//see PRM_Custom1.txt (in the same dir as this file) for the map file syntax...
 
-		return; //returns nothing caz void.
+		if (fileStart == false) {
+			Debug.LogError("Not a valid Map File: No \"Map Begin\" token: \'{\'.");
+
+		}
+
+
+		return; 
 	}
 
 
 
 
 
-	// Use this for initialization
+	// Start is called when the script is loaded
 	void Start () {
 		fname = "Assets\\Maps\\PRM_Custom1.txt"; //file name, but I want this to not be built into the exe in the future. How do?
-		file = new StreamReader(fname);
-		//file = new FileStream(fname, FileMode.Open, FileAccess.Read); //is this correct? I sm trying to open a file (obv).
-		CreateMap(file);
+		file = new StreamReader(fname); //loads file to be read
+		CreateMap(file); //does all the important things like spawning stuff and stuff (also things)
 
 		return;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		//nothing I need to do is called once per frame, afaik.
 		return;
 	
 	}
