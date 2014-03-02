5f323e49d4aa7876a4c7b5487cff271400cd0b0d
BananasGoMoo <snowballs3776573@aol.com>
2014-03-01 20:59:43 -0800

    more formatting lol

--- Assets/Maps/MapGen.cs
+++ Assets/Maps/MapGen.cs
@@ -2,7 +2,7 @@
 using System.Collections; //this was pre-included. IDK what it is.
 using System.IO; //for files and stuff
 
-public class MapGen : MonoBehaviour { //pre-generated class
+public class MapGen:MonoBehaviour { //pre-generated class
 
 		/* Globals: 
 	 Only has one file path for now, 
@@ -12,94 +12,96 @@ public class MapGen : MonoBehaviour { //pre-generated class
 	 that users can edit.
 	*/
 
+	string fname;
+	System.IO.StreamReader file;
+	string tokens = "@#/\\{};|&:";
+	int checkToken = -1;
+	
 
-		string fname;
-		System.IO.StreamReader file;
-		string tokens = "@#/\\{};|&:";
-		int checkToken = -1;
+	//this will place the blocks/prefabs for the generator.
+	bool PlaceBlocks(string prefabName, float x, float y, float z) {
+			GameObject block = (GameObject)Instantiate(Resources.Load(prefabName)); //Apparently returns 'null' if no prefab of that name/path exists.
+			if(block == null) { 
+					Debug.LogError("Error: Prefab name is invalid. Prefab not found.");
+					return false;
+			} else {
+					//PLACE BLOCKS!!
 
-		//this will place the blocks/prefabs for the generator.
-		bool PlaceBlocks (string prefabName, float x, float y, float z) {
-				GameObject block = (GameObject)Instantiate (Resources.Load (prefabName)); //Apparently returns 'null' if no prefab of that name/path exists.
-				if (block == null) { 
-						Debug.LogError ("Error: Prefab name is invalid. Prefab not found.");
-						return false;
-				} else {
-						//PLACE BLOCKS!!
+					block.transform.position.Set(x, y, z);
+			}
+			//if placed correctly (no issue finding the prefab, numbers are valid, etc.) this will return true.
+			return true;
+	}
 
-						block.transform.position.Set (x, y, z);
-				}
-				//if placed correctly (no issue finding the prefab, numbers are valid, etc.) this will return true.
-				return true;
-		}
+	public void CreateMap(StreamReader mapFile) { //this will generate the map. will call PlaceBlocks every time it needs to.
 
-		public void CreateMap (StreamReader mapFile) { //this will generate the map. will call PlaceBlocks every time it needs to.
-
-				//!!! See PRM_Custom1.txt (in the same dir as this file) for the map file syntax...
-		
-				string input;
-				string currentText;
-				System.IO.StreamReader ss = mapFile;
-				bool mapStart = false;
-				while ((input = ss.ReadLine()) != null) { //While not EOF
-						checkToken = input.IndexOfAny (tokens.ToCharArray ()); //check for any key character
-						if (checkToken == -1) { //No keywords found
-								Debug.Log ("No matching tokens left!");
-								continue;
-						}
-						currentText = input.Substring (checkToken); //substring starting with character found
-
-						if (mapStart == false) { //have I found the '{' character yet?
-								while (!currentText[0].Equals('{')) { //while its not found
-										checkToken = input.IndexOfAny (tokens.ToCharArray ()); //get first char
-										if (checkToken == -1) { //No more keywords found
-												Debug.Log ("No \'(\' token found yet! Reading next line...");
-												break;
-										} else { //now, look for a '{'
-												currentText = input.Substring (checkToken);
-												if (currentText [0].Equals ('{')) { //Found one!
-														mapStart = true;
-												}
-
-										}
-
-								} // end of "check for first '{' character" section
-
-
-						} else if (mapStart == true) { //now look for anything else
-								checkToken = input.IndexOfAny (tokens.ToCharArray ());
-
-						}
+			//!!! See Map FIle Syntax Guide.txt (in the same dir as this file) for the map file syntax...
+			
+			string input;
+			string currentText;
+			System.IO.StreamReader ss = mapFile;
+			bool mapStart = false;
 
 			
-				}
+			while((input = ss.ReadLine()) != null) { //While not EOF
+					checkToken = input.IndexOfAny(tokens.ToCharArray ()); //check for any key character
+					if(checkToken == -1) { //No keywords found
+							Debug.LogError("No matching tokens left!");
+							continue;
+					}
+					currentText = input.Substring(checkToken); //substring starting with character found
 
-				if (mapStart == false) {
-						Debug.LogError ("Not a valid Map File: No \"Map Begin\" token: \'{\'.");
+					if(mapStart == false) { //have I found the '{' character yet?
+							while(!currentText[0].Equals('{')) { //while its not found
+									checkToken = input.IndexOfAny(tokens.ToCharArray ()); //get first char
+									if(checkToken == -1) { //No more keywords found
+											Debug.Log("No \'(\' token found yet! Reading next line...");
+											break;
+									} else { //now, look for a '{'
+											currentText = input.Substring(checkToken);
+											if(currentText [0].Equals('{')) { //Found one!
+													mapStart = true;
+											}
 
-				}
+									}
+
+							} // end of "check for first '{' character" section
 
 
-				return; 
-		}
+					} else if(mapStart == true) { //now look for anything else
+							checkToken = input.IndexOfAny(tokens.ToCharArray ());
+
+					}
+
+		
+			}
+
+			if(mapStart == false) {
+					Debug.LogError("Not a valid Map File: No \"Map Begin\" token: \'{\'.");
+
+			}
+
+
+			return; 
+	}
 
 
 
 
 
-		// Start is called when the script is loaded
-		void Start () {
-				fname = "Assets\\Maps\\PRM_Custom1.txt"; //file name, but I want this to not be built into the exe in the future. How do?
-				file = new StreamReader (fname); //loads file to be read
-				CreateMap (file); //does all the important things like spawning stuff and stuff (also things)
+	// Start is called when the script is loaded
+	void Start() {
+			fname = "Assets\\Maps\\PRM_Custom1.txt"; //file name, but I want this to not be built into the exe in the future. How do?
+			file = new StreamReader(fname); //loads file to be read
+			CreateMap(file); //does all the important things like spawning stuff and stuff (also things)
 
-				return;
-		}
-	
-		// Update is called once per frame
-		void Update () {
-				//nothing I need to do is called once per frame, afaik.
-				return;
-	
-		}
+			return;
+	}
+
+	// Update is called once per frame
+	void Update() {
+			//nothing I need to do is called once per frame, afaik.
+			return;
+
+	}
 }
