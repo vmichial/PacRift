3e62004c1d926dcb488656edfeb9ea5aa21f8865
BananasGoMoo <snowballs3776573@aol.com>
2014-03-01 18:03:08 -0800

    Changed code format
    going to finish this tonight hopefully.

--- Assets/Maps/MapGen.cs
+++ Assets/Maps/MapGen.cs
@@ -2,8 +2,7 @@
 using System.Collections; //this was pre-included. IDK what it is.
 using System.IO; //for files and stuff
 
-public class MapGen : MonoBehaviour
-{ //pre-generated class
+public class MapGen : MonoBehaviour { //pre-generated class
 
 		/* Globals: 
 	 Only has one file path for now, 
@@ -12,16 +11,16 @@ public class MapGen : MonoBehaviour
 	 and then 2 or 3 customizable maps
 	 that users can edit.
 	*/
-	
+
+
 		string fname;
 		System.IO.StreamReader file;
 		string tokens = "@#/\\{};|&:";
 		int checkToken = -1;
 
 		//this will place the blocks/prefabs for the generator.
-		bool PlaceBlocks (string prefab, float x, float y, float z)
-		{
-				GameObject block = (GameObject)Instantiate (Resources.Load (prefab)); //Apparently returns 'null' if no prefab of that name/path exists.
+		bool PlaceBlocks (string prefabName, float x, float y, float z) {
+				GameObject block = (GameObject)Instantiate (Resources.Load (prefabName)); //Apparently returns 'null' if no prefab of that name/path exists.
 				if (block == null) { 
 						Debug.LogError ("Error: Prefab name is invalid. Prefab not found.");
 						return false;
@@ -34,15 +33,14 @@ public class MapGen : MonoBehaviour
 				return true;
 		}
 
-		public void CreateMap (StreamReader mapFile)
-		{ //this will generate the map. will call PlaceBlocks every time it needs to.
+		public void CreateMap (StreamReader mapFile) { //this will generate the map. will call PlaceBlocks every time it needs to.
 
 				//!!! See PRM_Custom1.txt (in the same dir as this file) for the map file syntax...
 		
 				string input;
 				string currentText;
 				System.IO.StreamReader ss = mapFile;
-				bool fileStart = false;
+				bool mapStart = false;
 				while ((input = ss.ReadLine()) != null) { //While not EOF
 						checkToken = input.IndexOfAny (tokens.ToCharArray ()); //check for any key character
 						if (checkToken == -1) { //No keywords found
@@ -51,7 +49,7 @@ public class MapGen : MonoBehaviour
 						}
 						currentText = input.Substring (checkToken); //substring starting with character found
 
-						if (fileStart == false) { //have I found the '{' character yet?
+						if (mapStart == false) { //have I found the '{' character yet?
 								while (!currentText[0].Equals('{')) { //while its not found
 										checkToken = input.IndexOfAny (tokens.ToCharArray ()); //get first char
 										if (checkToken == -1) { //No more keywords found
@@ -60,7 +58,7 @@ public class MapGen : MonoBehaviour
 										} else { //now, look for a '{'
 												currentText = input.Substring (checkToken);
 												if (currentText [0].Equals ('{')) { //Found one!
-														fileStart = true;
+														mapStart = true;
 												}
 
 										}
@@ -68,7 +66,7 @@ public class MapGen : MonoBehaviour
 								} // end of "check for first '{' character" section
 
 
-						} else if (fileStart == true) { //now look for anything else
+						} else if (mapStart == true) { //now look for anything else
 								checkToken = input.IndexOfAny (tokens.ToCharArray ());
 
 						}
@@ -76,7 +74,7 @@ public class MapGen : MonoBehaviour
 			
 				}
 
-				if (fileStart == false) {
+				if (mapStart == false) {
 						Debug.LogError ("Not a valid Map File: No \"Map Begin\" token: \'{\'.");
 
 				}
@@ -90,8 +88,7 @@ public class MapGen : MonoBehaviour
 
 
 		// Start is called when the script is loaded
-		void Start ()
-		{
+		void Start () {
 				fname = "Assets\\Maps\\PRM_Custom1.txt"; //file name, but I want this to not be built into the exe in the future. How do?
 				file = new StreamReader (fname); //loads file to be read
 				CreateMap (file); //does all the important things like spawning stuff and stuff (also things)
@@ -100,8 +97,7 @@ public class MapGen : MonoBehaviour
 		}
 	
 		// Update is called once per frame
-		void Update ()
-		{
+		void Update () {
 				//nothing I need to do is called once per frame, afaik.
 				return;
 	
