c2329f4e07778c47e447d184bb7fd732f8cc56e0
VMichial <VMichial@gmail.com>
2014-03-10 11:49:35 -0700

    GUI moving along
    Finished the Block code.
    Implementing the GUI controller along with the Block requesting feature.
    Algorithmic layout seems fine on first pass. Need to test the request
    feature with a gui element.
    
    NOTE: GUI elements should have a vector3 array for positions of all
    blocks and a Quaternion Array for all block rotations.
    
    Learn to use Lists.

---
+++ Assets/GUI/Frame.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Frame just make a giant box out of blocks
+//it needs to know what kind of blocks to use 
+//for widt and length. After that just how many
+//it will then spawn some at itself and move them where needed
+public class Frame : MonoBehaviour {
+
+	public int height = 0;//how many blocks high will te frame be?
+	public int width = 0;//how many blocks wide will the frame be?
+
+	private GameObject [] myBlocks;//arry to hold rferences to the blocks
+								   //that make up his particuar frame
+
+	public Length wBlock = Length.None;//wBlock tells you what kind of block to use
+	public Length hBlock = Length.None;//so does hBlock.
+
+	public bool activted = false; //if the frame isn't activated, itwill not do something...
+
+	 
+
+	public GameObject [] Bars;//this holds the prefabs of the blocks
+
+	// Use this for initialization
+	void Start () {
+		//first initialize the bars array so you can insantiate prefabs
+		Bars = new GameObject[3]{
+			Resources.Load<GameObject>("Full_Block"),
+			Resources.Load<GameObject>("Half_Block"),
+			Resources.Load<GameObject>("Quarter_Block")
+		};
+
+		//if the height/width wasn't set, at least set it to 1 so you have something
+		if(height == 0){height = 1;}
+		if(width == 0){width = 1;}
+
+		//if the block Lengths weren't selected then, at least make them quaters to see
+		if(wBlock == Length.None){wBlock = Length.Quarter;}
+		if(hBlock == Length.None){wBlock = Length.Quarter;}
+
+		myBlocks = new GameObject[(2*(width+height))];
+
+		switch(wBlock){
+		case Length.Full:
+			for(int i = 0; i<width; i++){
+
+			}break;
+		case Length.Half:
+			for(int i = 0; i<width; i++){
+				
+			}break;
+		case Length.Quarter:
+			for(int i = 0; i<width; i++){
+				
+			}break;
+		}
+
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+			//a loop to reasses the size of the frame. If they differ then they were changed
+			//go and make the changes to the frameso they are reflected.
+	}
+}
--- Assets/GUI/GUI Definitions.cs
+++ Assets/GUI/GUI Definitions.cs
@@ -16,60 +16,6 @@ enum Type{
 };
 
 public enum Length{
-	Full, Half, Quarter
+	None, Full, Half, Quarter
 };
 
-public class Letter{
-
-}
-
-public class Block{
-	public Length L;
-	public bool inUse;
-	public GameObject owner;
-	public GameObject me;
-
-	public Block(Length l,GameObject Parent){
-		L = l;
-		inUse=false;
-		owner = Parent;
-		me = new GameObject();
-		me.AddComponent("MeshFilter");
-		me.AddComponent("BoxCollider");
-		me.AddComponent("MeshRenderer");
-		me.AddComponent("Rigidbody");
-	}
-
-
-}
-
-//GUIs are made up of objects like buttons and line Edits.
-//This is done with a parent child system. Each object will
-//be positioned based on the location of it's parent if it has one.
-//it only needs to know how many of each block it needs, then you 
-//would redefine the functions necessary
-public abstract class GUI_Obj{
-	public bool active;		//active signals existing or not
-	public bool selected;	//is it selected
-	private Type type;		//type of gui element
-	private GUI_Obj parent;	//who am i a child of
-	public GUI_Obj [] children;	//who are my children
-
-	public int numFull;		//num of large blocks
-	public int numHalf;		//num of half blocks
-	public int numQuarter;	//num of quarter blocks
-}
-
-//the frame exists to make all of the elements inside be 
-//contained within itself, nd be operated on relative to itself
-//any options are set niside of frame options
-public class Frame_Options{
-	public int scale;
-}
-
-public class GUI_Frame{
-	public Frame_Options ops;
-	public int sides;
-	public int numL;
-
-}
