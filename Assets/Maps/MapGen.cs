6a0f3d050413a1bb769023ef0185b70a2332ef57
BananasGoMoo <snowballs3776573@aol.com>
2014-03-12 18:29:45 -0700

    Nearly done with MapGen
    Just need to do the Place Blocks switch.
    
    Also: block comments are annoying. They are now banned. :)

--- Assets/Maps/MapGen.cs
+++ Assets/Maps/MapGen.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine; //unity stuff
 using System.Collections; //this was pre-included. IDK what it is.
+using System.Collections.Generic; //for List<T>: Its basically a C++ vector, but in C#.
 using System.IO; //for files and stuff
 
 public class MapGen:MonoBehaviour { //pre-generated class
@@ -14,8 +15,9 @@ public class MapGen:MonoBehaviour { //pre-generated class
 
 	string fname;
 	System.IO.StreamReader file;
-	string tokens = "@#/\\{};|&:";
+	string tokens = "@#{};|&:%";
 	int checkToken = -1;
+	List<GameObject> placedBlocks = new List<GameObject>(); //will contain a list of all blocks placed, if they need to be accessed for some rasin later.
 
 	//---------------------------------------------------FUNCTIONS---------------------------------------------------\\
 	//---------------------------------------------------------------------------------------------------------------\\
@@ -27,25 +29,34 @@ public class MapGen:MonoBehaviour { //pre-generated class
 	bool PlaceBlocks(string prefabName, float x, float y, float z) {
 		GameObject block = (GameObject)Instantiate(Resources.Load(prefabName)); //Apparently returns 'null' if no prefab of that name/path exists.
 		if(block == null) { 
-			Debug.LogError("Error: Prefab name is invalid. Prefab not found.");
+			Debug.Log("<color=red>Error: Prefab name is invalid. Prefab not found.</color>");
 			return false;
 		} else {
-			//PLACE BLOCKS!!
-			
+			//PLACE BLOCKS!!			
 			block.transform.position.Set(x, y, z);
+			placedBlocks.Add(block);
 		}
 		//if placed correctly (no issue finding the prefab, numbers are valid, etc.) this will return true.
 		return true;
 	}
 
-	//FindKeyChar: *HELPER FXN*: This finds one of the syntax tokens.
+	//GetFoundChar: *HELPER FXN*: This finds one of the syntax tokens.
 	//------------
-	string FindKeyChar(string curLine){
-		string keyChar;  //for rturning the found character.
-		keyChar = curLine.Substring(checkToken,1);
-		checkToken++;
+	string GetFoundChar(string curLine){
+		string keyChar;  //for returning the found character.
+		if(curLine == null){
+			return "#";
 
-		return keyChar;
+		}
+		if(curLine.Length > checkToken){
+			keyChar = curLine.Substring(checkToken,1);
+			checkToken++;
+			return keyChar;
+		} else {
+			return "#";
+
+		}
+
 	}
 
 	//CreateMap: Main function to spawn the map described by the map file
@@ -60,78 +71,128 @@ public class MapGen:MonoBehaviour { //pre-generated class
 		bool mapEnd = false;
 		int mapSpawnG = 0;
 		int mapSpawnP = 0;
+		string mapName = "Default Map Name";
 		string testChar ="";
+		int lastX = 0;
+		int lastY = 0;
+		int lastZ = 0;
+		string lastObj = "";
+
 
 		while((input = ss.ReadLine()) != null) { //While not EOF
 			checkToken = input.IndexOfAny(tokens.ToCharArray()); //check for any key character
 
-			if(checkToken != -1) {
-				testChar = FindKeyChar(input);
-				Debug.Log ("testChar = " + testChar);
+			while(checkToken != -1) {
+				testChar = GetFoundChar(input);
+				Debug.Log ("<color=orange>testChar = </color>" + testChar);
 				switch(testChar) {
+
+					//"Map Begin"\\
 					case "{":
 						if(!mapStart) {
 							mapStart = true;
-							Debug.Log("Found the start of the map!");
+							Debug.Log("<color=green>Found the start of the map!</color>");
+							string temporary = input.Substring(checkToken);
+							checkToken = temporary.IndexOfAny(tokens.ToCharArray());
+							input = temporary;
 
 						} else if(mapStart) {
-							Debug.LogWarning("Invalid character: \"Map Begin\" character can only be used once per file!");
+							Debug.Log("<color=red>Invalid character: </color> <color=teal>\"Map Begin\" character can only be used once per file!</color>");
+							return;
 						}
 						break;
 
+					//"Map End"\\
 					case "}":
 						if(!mapStart){
-
+							Debug.Log("<color=red>Invalid character: </color> <color=teal>\"Map Begin\" character has not appeared yet!</color>");
+							return;
 
 						} else if(mapStart) {
-
-
+							mapEnd = true;
+							checkToken = -1;
+							Debug.Log("<color=green>Map finished generating!</color>");
+							break;
 						}
 						break;
 
+					//"Map Name"\\
 					case "@":
+						string tmp1 = input.Substring(checkToken);
+						string tmp2 = "";
+						Debug.Log("<color=orange>tmp1 = </color>" + tmp1);
+						checkToken = tmp1.IndexOfAny(tokens.ToCharArray());
+						if(checkToken == -1){
+							Debug.Log("<color=red>Error: Single \'@\' symbol found. Must be paired.</color> <color=purple>Line:</color>" + input);
+							return;
+						} else {
+							tmp2 = GetFoundChar(tmp1);
+							if (tmp2 != "@"){
+								Debug.Log("<color=red>Error: Cannot have keywords in map name.</color> <color=teal>Keywords are: { } # @ \\ / ; | & : % and the character found is: </color>" + tmp1);
+								return;
+							} else {
+								mapName = tmp1.Substring(0 , checkToken-1);
+								Debug.Log("<color=orange>Map Name: </color>" + mapName);
+								checkToken+=2;
+								tmp1 = input.Substring(checkToken);
+								checkToken = tmp1.IndexOfAny(tokens.ToCharArray());
+								input = tmp1;
+							}
+						}
 						break;
-
+					
+					//"Comment"\\
 					case "#":
+						checkToken = -1;
 						break;
 
-					case "/":
-						break;
-
-					case "\\":
-						break;
-
+					//"Misused symbol: New Object"\\
 					case ";":
-						break;
+						Debug.Log("<color=red>Error: Misplaced semicolon.</color> <color=purple>Line: </color>" + input);
+						return;
 
+					//"Misused symbol: New Object"\\
 					case "|":
-						break;
+						Debug.Log("<color=red>Error: Misplaced pipe symbol.</color> <color=purple>Line: </color>" + input);
+						return;
 
+					//"Misused symbol: New Object"\\
 					case "&":
+						Debug.Log("<color=red>Error: Misplaced ampersand.</color> <color=purple>Line: </color>" + input);
+						return;
+					
+					//"New Object"\\
+					case ":":
+
 						break;
 
-					case ":":
+					//"Misused symbol: New Object"\\
+					case "%":
+						Debug.Log("<color=red>Error: Misplaced percent symbol.</color> <color=purple>Line: </color>" + input);
+						return;
+					//Just in case I get a non-keyword char
+					default:
+
 						break;
 
 				} //switch End
 
-				Debug.Log ("SS.ReadLine is " + "\"" + input + "\"");
+				Debug.Log ("<color=purple>SS.ReadLine is </color>" + "<color=purple>\"</color>" + input + "<color=purple>\"</color>");
 
-			} else {
-				Debug.Log("No character on this line.");
-				Debug.Log ("SS.ReadLine is " + "\"" + input + "\"");
-			} //check line End
+			} 
+			Debug.Log("<color=aqua>No more characters on this line.</color>");
+			Debug.Log ("<color=purple>SS.ReadLine is </color>" + "<color=purple>\"</color>" + input + "<color=purple>\"</color>"); //check line End
 
 		} //read file End
 		if (mapEnd && mapStart && (mapSpawnG==4) && (mapSpawnP >=1 && mapSpawnP <=3)) { //check if all required map parts exist!
-			Debug.Log("Map generation successful! No errors to report!");
+			Debug.Log("<color=lime>Map generation successful! No errors to report!</color>");
 
 		} else {
-			Debug.LogWarning("Error, map generated incorrectly! Values:" 
-			                 + "Map begin symbol: " + mapStart 
-			                 + ", Map end symbol: " + mapEnd 
-			                 + ", Number of Ghost Spawns [must be 4]: " + mapSpawnG
-			                 + ", Number of Pac Spawns [must be 1-3]" + mapSpawnP);
+			Debug.Log("<color=red>Error, map generated incorrectly! Values: </color>" 
+			                 + "<color=orange>Map begin symbol: </color>" + mapStart 
+			                 + "<color=orange>, Map end symbol: </color>" + mapEnd 
+			                 + "<color=orange>, Number of Ghost Spawns [must be 4]: </color>" + mapSpawnG
+			                 + "<color=orange>, Number of Pac Spawns [must be 1-3]: </color>" + mapSpawnP);
 
 		}
 		ss.Close();
@@ -159,52 +220,3 @@ public class MapGen:MonoBehaviour { //pre-generated class
 		
 	}
 };
-
-//------------------------------------------BROKEN CODE------------------------------------------\\
-
-/*					if(checkToken == -1) { //No keywords found
-						Debug.Log("No matching tokens left!");
-						continue;
-				}
-				currentText = input.Substring(checkToken); //substring starting with character found
-
-				if(mapStart == false) { //have I found the '{' character yet?
-						while(!currentText[0].ToString().Equals("{")) { //while its not found
-								Debug.Log (currentText[0].ToString());
-								checkToken = input.IndexOfAny(tokens.ToCharArray ()); //get first char
-								if(checkToken == -1) { //No more keywords found
-										Debug.Log("No \'(\' token found yet! Reading next line...");
-										break;
-								} else { //now, look for a '{'
-										currentText = input.Substring(checkToken);
-										if(currentText[0].Equals('{')) { //Found one!
-												currentText = input.Substring(checkToken);
-												mapStart = true;
-												Debug.Log("Found the beginning of the map.");
-										} else {
-											currentText = input.Substring(checkToken++);
-										}
-
-								}
-
-						} // end of "check for first '{' character" section
-
-
-				} else if(mapStart == true) { //now look for anything else
-						checkToken = input.IndexOfAny(tokens.ToCharArray ());
-						currentText = input.Substring(checkToken);
-						switch (currentText) {
-							
-							case "}":
-								break;
-
-						}
-
-	
-				}
-
-		if(mapStart == false) {
-				Debug.LogWarning("Not a valid Map File: No \"Map Begin\" token: \'{\'.");
-				return;
-
-		}*/
