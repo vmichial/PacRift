f1e469011488a440042b477374bcff12a44624b8
VMichial <VMichial@gmail.com>
2014-04-05 18:08:53 -0700

    wut

---
+++ Assets/Actors/Pellet/Script/Pellet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pellet : MonoBehaviour {
+
+	private float speed = 0.05f;
+	private bool up = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(){
+		Destroy(gameObject);
+	}
+}
